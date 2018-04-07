8db10c5a680501ac293dfbfd175772548630a8c9
ChaitanyaKulkarni28 <chaitanyakulkarni28@gmail.com>
2018-04-06 21:30:25 -0500

    Given a list of numbers finding minimum value, maximum value and median value.

--- MapReduce/minMaxMedian.java
+++ MapReduce/minMaxMedian.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.fs.Path;  
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
